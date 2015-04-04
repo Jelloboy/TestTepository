e14061557dfe3e12af662fe02292839e32635d61
Jelle Spreeuwenberg <jelles2002@gmail.com>
2015-04-04 13:03:30 +0200

    Added a comment for better readability

--- src/com/fahno/escape/Launcher.java
+++ src/com/fahno/escape/Launcher.java
@@ -4,6 +4,6 @@ public class Launcher {
 	public static void main(String[] args) {
 		Game game = new Game(640, 480, "Escape");
 		game.start();
-		//Amazing Launcher wow 23
+		//Amazing Launcher wow 234
 	}
 }
